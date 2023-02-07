8977dd7fe0ff0e2eaa1310ab7f63c7955a513853
nuxtnode <nuxtnode@gmail.com>
2023-02-07 15:26:52 +0800

    nice

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,8 @@
 export default {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': '',
-        'database': 'full_nuxt'
+        'host': 'us-cdbr-east-06.cleardb.net',
+        'user': 'b8b776be6f6ecf',
+        'password': '82332229',
+        'database': 'heroku_1d45d74009eeb1d'
     }
 }
